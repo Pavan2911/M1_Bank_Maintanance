dd93843d8dc3c4951ce471b197270df999119aa1
Pavan2911 <ugs18161_cse.pavan@cbit.org.in>
2022-04-01 16:04:41 +0530

    commit

---
+++ 3_Implementation/Inc/Bank_Main.h
@@ -0,0 +1,24 @@
+/**
+ * @file Bank_Main.h
+ * @author Pavan Kumar (ugs18161_cse.pavan@cbit.org.in)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _cbank_
+void admin();
+void main_screen();
+void bank_menu();
+void create_new_account();
+void edit_details();
+void compute_transaction();
+float compute_interest(float t, float amount, int rate);
+void compute_delay(int j);
+void view_list();
+void erase_details();
+void see_details();
+void close_application();
+#endif
